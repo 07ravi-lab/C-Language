c132cc84a045e6103fb0cb05cf55179c36e613ce
Ravi Raj <msd7ravis@gmail.com>
2025-05-08 21:10:50 +0530

    Update primenumberuse break.c

--- loops/primenumberuse break.c
+++ loops/primenumberuse break.c
@@ -1,38 +1,27 @@
-{
-  "message": {
-    "generalPurposeCard": {
-      "layout": {
-        "cardOrientation": "VERTICAL"
-      },
-      "content": {
-        "title": "Get IHCL Silver Membership + 30% off on your next stay with NeuCard!",
-        "description": "\nTata Neu HDFC Bank Credit Card!\n\nMaximize your Rewards this festive season with NeuCard at ZERO JOINING FEE. Enjoy up to: \n💸 10% savings on Tata Brands \n🛫 12 Complimentary Lounge access annually \n💳 1.5% savings on UPI payments\n\n*Kindly ignore this message if you are an existing NeuCard Holder*",
-        "media": {
-          "mediaUrl": "https://rbm-ap.storage.googleapis.com/456340535902/m5nks2h6UakG4VmZjpmlcCcw",
-          "mediaContentType": "image/jpeg",
-          "mediaFileSize": 121906,
-          "height": "MEDIUM_HEIGHT",
-          "contentDescription": "dec10.jpg",
-          "dimensions": {
-            "width": 1274,
-            "height": 546
-          }
-        },
-        "suggestions": [{
-          "action": {
-            "displayText": "Apply Now",
-            "postback": {
-              "data": "{\"orgMsgId\":\"dc2870fa-5ae0-4ead-8b34-4b16419473fb\",\"orgMsgSendTime\":\"2025-01-29T15:20:50.035996Z\",\"campaignId\":\"6799cf2694b1f73f7ed15a06\",\"postback\":\"SA1L1C1\"}"
-            },
-            "urlAction": {
-              "openUrl": {
-                "url": "https://www.tatadigital.com/v2/finance/creditcard/product-detail?utm_source\u003dPartnerships_external\u0026utm_medium\u003dJBM\u0026utm_campaign\u003dIHCL",
-                "application": "browser"
-              }
-            }
-          }
-        }]
-      }
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int num) {
+    if (num <= 1)
+        return false;
+
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return false;
     }
-  }
+
+    return true;
+}
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (isPrime(number))
+        printf("%d is a prime number.\n", number);
+    else
+        printf("%d is not a prime number.\n", number);
+
+    return 0;
 }
